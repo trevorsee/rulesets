24988f47465178944fe455445435e9223d6f8c87
trevorsee <tcarr@mica.edu>
2017-09-11 21:22:54 -0500

    check if options exist

--- index.js
+++ index.js
@@ -5,7 +5,20 @@ function newRuleSet() {
   }
 };
 
+function addOption(title, rs) {
+	if ( !rs.options[title] ) {
+    	rs.options[title] = {
+    	isParentTo: [],
+    	isDependentOn: [],
+    };
+  }
+}
+
 function addDependency(o, t, rs){
+  // check that option exists
+  addOption(o, rs);
+  // check that target-option exists
+  addOption(t, rs);
 }
 
 function addConflict(o, t, rs){
@@ -15,4 +28,7 @@ function checkCoherence(rs){
 }
 
 let s = newRuleSet();
+
+addDependency('option A', 'option B', s)
+
 console.log(s.options);
